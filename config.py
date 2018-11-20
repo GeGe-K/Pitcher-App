93cee5829efeb664ab8d326c812ddfc13eda25f2
GeGe-K <gloriagivondo@gmail.com>
2018-11-20 12:35:07 +0300

    moved the email configutations into the config class.

--- config.py
+++ config.py
@@ -1,17 +1,16 @@
 import os
 
-#  email configurations
-EMAIL_SERVER = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
-EMAIL_USERNAME = os.environ.get("EMAIL_USERNAME")
-
-
 class Config:
     '''
     General configuration parent class
     '''
+    #  email configurations
+
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+    MAIL_USERNAME = os.environ.get("EMAIL_USERNAME")
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:maroon5@localhost/pitcher'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
