bf73bf2673846d36bf858b73bac8c9f463c08565
GeGe-K <gloriagivondo@gmail.com>
2018-11-20 08:20:41 +0300

    added SelectField to the imports.

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
+from wtforms import StringField,TextAreaField,SelectField,SubmitField
 from wtforms.validators import Required
 
 class CommentForm(FlaskForm):
