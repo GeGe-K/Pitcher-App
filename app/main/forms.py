563dbd4cf72588cd7c4309cd772a18745e011523
GeGe-K <gloriagivondo@gmail.com>
2018-11-20 05:30:06 +0300

    added a connent form.

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,9 +2,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField
 from wtforms.validators import Required
 
-class ReviewForm(FlaskForm):
-    title = StringField('Review title', validators = [Required()])
-    review = TextAreaField('Movie review')
+class CommentForm(FlaskForm):
+    title = StringField('Comment title', validators = [Required()])
+    review = TextAreaField('comment')
     submit = SubmitField('Submit')
 
 class UpdateProfile(FlaskForm):
